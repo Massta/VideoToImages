5a91384a6ccedd3325a52f3cb8b3fb69d0b16819
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2019-09-24 11:45:06 +0200

    Change form title

--- VideoToImages/Form1.Designer.cs
+++ VideoToImages/Form1.Designer.cs
@@ -167,7 +167,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Video To Images";
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
